fbba8e0fbcdb3b4db9af1bb222e463c4c2c1111b
Artur <rtoor.3io7@gmail.com>
2016-12-23 21:28:27 +0200

    Update example_test.js

--- test/example_test.js
+++ test/example_test.js
@@ -8,6 +8,6 @@ var branchUrl = 'http://cicdqaws-' + branchName + '.herokuapp.com'
 describe('Home page tests', function() {
     it('Header is correct', function() {
         browser.url(branchUrl)
-        browser.getText('h1').should.equal('My Site')
+        browser.getText('h1').should.equal('Other Side')
     });
 });
